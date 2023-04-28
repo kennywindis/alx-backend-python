e0bea28a557c8b527f2b9912067a532f1a5126e3
kennywindis <kennyphilip100@gmail.com>
2023-04-27 18:36:07 -0700

    va

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,12 +1,6 @@
-!/usr/bin/env python3
-""" Basic annotations - concat """
+#!/usr/bin/env python3
+"""concatenation"""
 
 def concat(str1: str, str2: str) -> str:
-    """concatenates two strings
-    Args:
-        str1 (str): string 1
-        str2 (str): string 2
-    Returns:
-        str: concatenated string
-    """
-    return str1 + str2
+    """concatenate two(2) strings"""
+     return str1 + str2
