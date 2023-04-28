d08e83ece8f459b240ba1a690260b9d5c843787c
kennywindis <kennyphilip100@gmail.com>
2023-04-27 17:43:57 -0700

    va

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Complex types - functions """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ function make_multiplier that takes a float multiplier as argument and returns a function that multiplies a float by multiplier. """
+    def fn(k: float):
+        return k * multiplier
+    return fn
