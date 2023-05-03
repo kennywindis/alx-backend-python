d21bc592a31e0ed468feb8062d32e2c7ef81478a
kennywindis <kennyphilip100@gmail.com>
2023-05-03 04:11:20 -0700

    ac

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-
-""" async generator """
-
+""" a coroutine called async_generator that takes no arguments. """
 import asyncio
 from typing import Generator
 import random
 
+
 async def async_generator() -> Generator[float, None, None]:
-    """ a coroutine with no args """
-    for k in range(10):
+    """ Async Generator """
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
